538540215c8f802f49899172f4fd130a7f8c11ff
assafmo <assaf.morami@gmail.com>
2019-04-07 16:43:30 +0300

    Updates

---
+++ precache-manifest.f022cac4222a871e0eb9bf39fc8727fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ea7582bd2388c26370ea9a9ef65749c",
+    "url": "/static/media/instagram-logo-balck-white.7ea7582b.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "651d6d6716dd39072458",
+    "url": "/static/js/main.b04d6510.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "651d6d6716dd39072458",
+    "url": "/static/css/main.64e8cd36.chunk.css"
+  },
+  {
+    "revision": "e963f6a54f1b3667fc5a6d925c9aad1f",
+    "url": "/index.html"
+  }
+];
